44bd81fb47857f4f8d488a15145c5715d69a828c
Micah Wedemeyer <micah@calculatedcombustion.com>
2010-06-05 18:47:42 -0400

    Updated Pay to latest API.

--- lib/remit2/operations/pay.rb
+++ lib/remit2/operations/pay.rb
@@ -1,31 +1,37 @@
 require 'remit2/common'
 
+# Updated for API Version 2008-09-17
 module Remit
   module Pay
     class Request < Remit::Request
       action :Pay
       parameter :caller_description
       parameter :caller_reference, :required => true
-      parameter :caller_token_id, :required => true
-      parameter :charge_fee_to, :required => true
+      parameter :charge_fee_to
       parameter :descriptor_policy, :type => Remit::RequestTypes::DescriptorPolicy
       parameter :marketplace_fixed_fee, :type => Remit::RequestTypes::Amount
       parameter :marketplace_variable_fee
       parameter :meta_data
       parameter :recipient_description
       parameter :recipient_reference
-      parameter :recipient_token_id, :required => true
+      parameter :recipient_token_id
       parameter :sender_description
       parameter :sender_reference
       parameter :sender_token_id, :required => true
       parameter :temporary_decline_policy, :type => Remit::RequestTypes::TemporaryDeclinePolicy
       parameter :transaction_amount, :type => Remit::RequestTypes::Amount, :required => true
       parameter :transaction_date
+      parameter :transaction_timeout_in_mins
     end
-
+    
+    class InnerResponse < Remit::BaseResponse
+      parameter :transaction_id
+      parameter :transaction_status
+    end
+    
     class Response < Remit::Response
-      parser :rexml
-      parameter :transaction_response, :namespace => 'ns3', :type => TransactionResponse
+      parameter :inner, :element => "PayResult", :type => InnerResponse
+      inner_parameters :transaction_id, :transaction_status
     end
 
     def pay(request = Request.new)
